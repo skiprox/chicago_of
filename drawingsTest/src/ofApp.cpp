8306ce6e204ed5c4c15dcde240d0b79688f84157
skiprox <seanmichaelscanlan@gmail.com>
2019-07-03 15:57:05 -0400

    Starting work on this branch

--- drawingsTest/src/ofApp.cpp
+++ drawingsTest/src/ofApp.cpp
@@ -6,14 +6,30 @@ void ofApp::setup(){
 	ofSetCircleResolution(100);
 	width = ofGetWidth();
 	height = ofGetHeight();
+	// 0
 	pts1.push_back(glm::vec2(50, height/2 - 50));
 	pts2.push_back(glm::vec2(50, height/2 + 50));
+	// 1
 	pts1.push_back(glm::vec2(width - 50, height/2 + 25));
 	pts2.push_back(glm::vec2(width - 50, height/2 - 25));
+	// 2
 	pts1.push_back(glm::vec2(width - 100, height/2 - 25));
 	pts2.push_back(glm::vec2(width - 50, height/2 - 25));
+	// 3
 	pts1.push_back(glm::vec2(width - 60, 50));
 	pts2.push_back(glm::vec2(width - 90, 50));
+	// 4
+	pts1.push_back(glm::vec2(width - 90, 50));
+	pts2.push_back(glm::vec2(width - 90, 80));
+	// 5
+	pts1.push_back(glm::vec2(120, 75));
+	pts2.push_back(glm::vec2(120, 55));
+	// 6
+	pts1.push_back(glm::vec2(120, 55));
+	pts2.push_back(glm::vec2(120, 75));
+	// 7
+	pts1.push_back(glm::vec2(120, 55));
+	pts2.push_back(glm::vec2(120, 75));
 }
 
 //--------------------------------------------------------------
@@ -30,7 +46,7 @@ void ofApp::draw(){
 	// 4) whether to include an arrow at the end
 	// 5) incrementer
 	// Draw the pointer
-	drawPointer(pts1, pts2, false, incrementer);
+	drawPointer(pts1, pts2, true, incrementer);
 	incrementer++;
 }
 
@@ -57,9 +73,55 @@ void ofApp::drawPointer(vector<glm::vec2> pts1, vector<glm::vec2> pts2, bool sho
 		path.draw();
 		path.moveTo(pts1[2]);
 		path.lineTo(pts2[2]);
-		float easedIncrement = quadEaseOut((incrementer - 200)/100.0);
+		float easedIncrement = quadEaseOut((incrementer - 300)/100.0);
 		path.lineTo(ofLerp(pts2[2].x, pts1[3].x, easedIncrement), ofLerp(pts2[2].y, pts1[3].y, easedIncrement));
 		path.lineTo(ofLerp(pts1[2].x, pts2[3].x, easedIncrement), ofLerp(pts1[2].y, pts2[3].y, easedIncrement));
+		path.close();
+		path.draw();
+	} else if (incrementer <= 400) {
+		path.moveTo(pts1[0]);
+		path.lineTo(pts2[0]);
+		path.lineTo(pts1[1]);
+		path.lineTo(pts2[1]);
+		path.close();
+		path.draw();
+		path.moveTo(pts1[2]);
+		path.lineTo(pts2[2]);
+		path.lineTo(pts1[3]);
+		path.lineTo(pts2[3]);
+		path.close();
+		path.draw();
+		path.moveTo(pts1[4]);
+		path.lineTo(pts2[4]);
+		float easedIncrement = quadEaseOut((incrementer - 400)/100.0);
+		path.lineTo(ofLerp(pts2[4].x, pts1[5].x, easedIncrement), ofLerp(pts2[4].y, pts1[5].y, easedIncrement));
+		path.lineTo(ofLerp(pts1[4].x, pts2[5].x, easedIncrement), ofLerp(pts1[4].y, pts2[5].y, easedIncrement));
+		path.close();
+		path.draw();
+	} else if (incrementer <= 450) {
+		path.moveTo(pts1[0]);
+		path.lineTo(pts2[0]);
+		path.lineTo(pts1[1]);
+		path.lineTo(pts2[1]);
+		path.close();
+		path.draw();
+		path.moveTo(pts1[2]);
+		path.lineTo(pts2[2]);
+		path.lineTo(pts1[3]);
+		path.lineTo(pts2[3]);
+		path.close();
+		path.draw();
+		path.moveTo(pts1[4]);
+		path.lineTo(pts2[4]);
+		path.lineTo(pts1[5]);
+		path.lineTo(pts2[5]);
+		path.close();
+		path.draw();
+		path.moveTo(pts1[6]);
+		path.lineTo(pts2[6]);
+		float easedIncrement = quadEaseOut((incrementer - 450)/100.0);
+		path.lineTo(ofLerp(pts2[6].x, pts1[7].x, easedIncrement), ofLerp(pts2[6].y, pts1[7].y, easedIncrement));
+		path.lineTo(ofLerp(pts1[6].x, pts2[7].x, easedIncrement), ofLerp(pts1[6].y, pts2[7].y, easedIncrement));
 		path.close();
 		path.draw();
 	} else {
@@ -73,6 +135,18 @@ void ofApp::drawPointer(vector<glm::vec2> pts1, vector<glm::vec2> pts2, bool sho
 		path.lineTo(pts2[2]);
 		path.lineTo(pts1[3]);
 		path.lineTo(pts2[3]);
+		path.close();
+		path.draw();
+		path.moveTo(pts1[4]);
+		path.lineTo(pts2[4]);
+		path.lineTo(pts1[5]);
+		path.lineTo(pts2[5]);
+		path.close();
+		path.draw();
+		path.moveTo(pts1[6]);
+		path.lineTo(pts2[6]);
+		path.lineTo(pts1[7]);
+		path.lineTo(pts2[7]);
 		path.close();
 		path.draw();
 		if (shouldHaveArrow) {
