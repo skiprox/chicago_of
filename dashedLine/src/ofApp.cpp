e907dd022cbce299eb707adef67f8d99155eeadb
skiprox <seanmichaelscanlan@gmail.com>
2019-07-17 18:21:07 -0400

    Updates to dashed and straight lines

--- dashedLine/src/ofApp.cpp
+++ dashedLine/src/ofApp.cpp
@@ -4,18 +4,20 @@
 void ofApp::setup(){
 	ofBackground(255, 255, 255);
 	ofSetCircleResolution(100);
+	float width = ofGetWidth();
+	float height = ofGetHeight();
 	incrementer = 0;
 	vector<std::array<glm::vec2, 2>> pts;
-	pts.push_back(array<glm::vec2, 2> {{glm::vec2(0, 0), glm::vec2(400, 400)}});
-	pts.push_back(array<glm::vec2, 2> {{glm::vec2(400, 400), glm::vec2(800, 100)}});
+	pts.push_back(array<glm::vec2, 2> {{glm::vec2(0, 0), glm::vec2(width/2.1, height/2.2)}});
+	pts.push_back(array<glm::vec2, 2> {{glm::vec2(width/2.1, height/2.2), glm::vec2(width/1.3, 100)}});
 	dashedLines.push_back(DashedLine(pts, 1.0, ofColor(240, 40, 20), 250, true));
 	pts.clear();
-	pts.push_back(array<glm::vec2, 2> {{glm::vec2(0, 800), glm::vec2(100, 400)}});
-	pts.push_back(array<glm::vec2, 2> {{glm::vec2(98, 398), glm::vec2(800, 600)}});
-	dashedLines.push_back(DashedLine(pts, 5.0, ofColor(240, 40, 20), 250, true));
+	pts.push_back(array<glm::vec2, 2> {{glm::vec2(0, height/1.1), glm::vec2(100, height/2)}});
+	pts.push_back(array<glm::vec2, 2> {{glm::vec2(98, height/2 - 2), glm::vec2(width/1.1, height/1.3)}});
+	dashedLines.push_back(DashedLine(pts, 4.0, ofColor(240, 40, 20), 250, true));
 	pts.clear();
-	pts.push_back(array<glm::vec2, 2> {{glm::vec2(800, 300), glm::vec2(200, 100)}});
-	pts.push_back(array<glm::vec2, 2> {{glm::vec2(202, 102), glm::vec2(100, 600)}});
+	pts.push_back(array<glm::vec2, 2> {{glm::vec2(width/1.1, height/3), glm::vec2(200, 100)}});
+	pts.push_back(array<glm::vec2, 2> {{glm::vec2(202, 102), glm::vec2(100, height/1.6)}});
 	dashedLines.push_back(DashedLine(pts, 10.0, ofColor(240, 40, 20), 250, true));
 }
 
--- lineArrows/src/ofApp.cpp
+++ lineArrows/src/ofApp.cpp
@@ -4,23 +4,29 @@
 void ofApp::setup(){
 	ofBackground(255);
 	ofSetCircleResolution(100);
+	float width = ofGetWidth();
+	float height = ofGetHeight();
 	vector<std::array<glm::vec2, 2>> pts;
-	pts.push_back(array<glm::vec2, 2> {{glm::vec2(0, 0), glm::vec2(400, 400)}});
-	pts.push_back(array<glm::vec2, 2> {{glm::vec2(400, 400), glm::vec2(800, 100)}});
+	pts.push_back(array<glm::vec2, 2> {{glm::vec2(0, 0), glm::vec2(width/2.0, height/2.0)}});
+	pts.push_back(array<glm::vec2, 2> {{glm::vec2(width/2.0, height/2.0), glm::vec2(width/1.2, 100)}});
 	lineArrows.push_back(LineArrow(pts, 1.0, ofColor(240, 40, 20), 250, true));
 	pts.clear();
-	pts.push_back(array<glm::vec2, 2> {{glm::vec2(0, 800), glm::vec2(100, 400)}});
-	pts.push_back(array<glm::vec2, 2> {{glm::vec2(98, 398), glm::vec2(800, 600)}});
-	lineArrows.push_back(LineArrow(pts, 5.0, ofColor(240, 40, 20), 250, true));
+	pts.push_back(array<glm::vec2, 2> {{glm::vec2(0, height/1.1), glm::vec2(100, height/2)}});
+	pts.push_back(array<glm::vec2, 2> {{glm::vec2(98, height/2 - 2), glm::vec2(width/1.1, height/1.6)}});
+	lineArrows.push_back(LineArrow(pts, 1.0, ofColor(240, 40, 20), 250, true));
 	pts.clear();
-	pts.push_back(array<glm::vec2, 2> {{glm::vec2(800, 300), glm::vec2(200, 100)}});
-	pts.push_back(array<glm::vec2, 2> {{glm::vec2(202, 102), glm::vec2(100, 600)}});
+	pts.push_back(array<glm::vec2, 2> {{glm::vec2(width/1.1, height/4), glm::vec2(200, 100)}});
+	pts.push_back(array<glm::vec2, 2> {{glm::vec2(202, 102), glm::vec2(100, height/1.6)}});
+	lineArrows.push_back(LineArrow(pts, 4.0, ofColor(240, 40, 20), 250, true));
+	pts.clear();
+	pts.push_back(array<glm::vec2, 2> {{glm::vec2(width, height), glm::vec2(200, 240)}});
+	lineArrows.push_back(LineArrow(pts, 4.0, ofColor(240, 40, 20), 250, true));
+	pts.clear();
+	pts.push_back(array<glm::vec2, 2> {{glm::vec2(200, height/1.1), glm::vec2(198, height/3)}});
 	lineArrows.push_back(LineArrow(pts, 10.0, ofColor(240, 40, 20), 250, true));
 	pts.clear();
-	pts.push_back(array<glm::vec2, 2> {{glm::vec2(800, 800), glm::vec2(200, 240)}});
-	lineArrows.push_back(LineArrow(pts, 10.0, ofColor(240, 40, 20), 250, true));
-	pts.clear();
-	pts.push_back(array<glm::vec2, 2> {{glm::vec2(200, 800), glm::vec2(198, 300)}});
+	pts.push_back(array<glm::vec2, 2> {{glm::vec2(width, 200), glm::vec2(100, height/1.4)}});
+	pts.push_back(array<glm::vec2, 2> {{glm::vec2(98, height/1.4 + 2), glm::vec2(width/1.3, height/1.1)}});
 	lineArrows.push_back(LineArrow(pts, 10.0, ofColor(240, 40, 20), 250, true));
 }
 
